552e919700808c28aa9cbace651f762ed4c8bc6f
tanahiro2010 <herentongkegu087@gmail.com>
2024-05-11 07:01:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,6 @@ async def on_ready():
                         developers.append(member.id)
                         break
             print(developers)
-        else:
-            print(f"{server.name} has {len(server.members)} members")
 
 
 def admin_control(msg):
@@ -32,21 +30,40 @@ def admin_control(msg):
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
-        print(f"{message.author.name} says '{message.content}'")
+        # print(f"{message.author.name} says '{message.content}'")
         return
-
     else:
         user_id = message.author.id
         if user_id in developers:
             admin_control(message)
 
-        if message.content == "!Zisty-projects":
+        if message.content == "!zisty-projects":
             print("Zisty projects")
-            await message.channel.send("Zisty projects")
+            await message.channel.send("現在公開されているZistyのprojectは以下の道りです")
             projects = json.loads(requests.get('https://api.github.com/users/zisty-h/repos').content)
-            print(projects)
+            send_text = ''
+            for project in projects:
+                send_text += f'# {project["name"]}\ndescription: {project["description"]}\nurl: {project["html_url"]}\n'
+            await message.channel.send(send_text)
+        elif message.content == "!zisty-members":
+            await message.channel.send("Please wait.\nGetting members...")
+            members = bot.get_guild(1185947730363826236).get_role(1187750962694193243).members
+            await message.channel.send("現在参加しているZistyの開発者は以下のユーザーです")
+            for member in members:
+                await message.channel.send(f'## {member.display_name}\nID: {member.name}...(User_id: {member.id}) ')
+        elif message.content == "!zisty-join":
+            server = bot.get_guild(1185947730363826236)
+            link = await server.get_channel(1187754444687949986).create_invite()
+            print(link)
+            await message.channel.send(f'Zisty join link: {link}')
 
+@bot.event
+async def on_member_join(member):
+    return
 
-
-
+@bot.event
+async def on_member_remove(member):
+    await member.remove_roles(member.guild.get_role(1187750962694193243))
+    print(member)
+    return
 bot.run(Discord_token)
